4975c359eed47053bcc9fe0320d16ce6f5517a39
Lewis Cavallo <40280564+LewisCav@users.noreply.github.com>
2018-10-26 19:03:43 +1000

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,20 @@
+window.onload = function()
+{
+    document.getElementById('pageheader').innerHTML = `<h1>Flash Imagery</h1>`;
+
+
+    document.getElementById('navsystem').innerHTML = `
+    <ul>
+        <a href="index.html"><li>Home</li></a>
+        <a href="about.html"><li>About Us</li></a>
+        <a href="portfolio.html"><li>Portfolio</li></a>
+        <a href="blog.html"><li>Blog</li></a>
+        <a href="contact.html"><li>Contact Us</li></a>
+    </ul>
+    `;
+
+    document.getElementById('pagefooter').innerHTML = `
+    <p>Copyright Text Text Text Text Text Text Text Text </p>
+    `;
+
+}
